f9fea57cbe89d412f10f7a09e6dfb1fce820271b
Kim <kimsaints15@gmail.com>
2024-03-18 15:43:00 +0800

    updates

--- lib/feed_tab.dart
+++ lib/feed_tab.dart
@@ -1,8 +1,13 @@
-// ignore_for_file: prefer_const_constructors, use_build_context_synchronously, prefer_final_fields
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,8 +24,9 @@ class MyApp extends StatelessWidget {
           buttonColor: Color.fromRGBO(230, 72, 111, 1),
           textTheme: ButtonTextTheme.primary,
         ),
-        colorScheme: ColorScheme.fromSwatch()
-            .copyWith(secondary: Color.fromRGBO(254, 173, 86, 1)),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Color.fromRGBO(254, 173, 86, 1),
+        ),
       ),
       home: FeedTab(),
     );
@@ -35,9 +41,21 @@ class FeedTab extends StatefulWidget {
 class _FeedTabState extends State<FeedTab> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final ScrollController _scrollController = ScrollController();
 
   TextEditingController _titleController = TextEditingController();
   TextEditingController _descriptionController = TextEditingController();
+  TextEditingController _photoController = TextEditingController();
+
+  // Map to store loaded images
+  final Map<String, ImageProvider> _imageCache = {};
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +70,7 @@ class _FeedTabState extends State<FeedTab> {
             return Center(child: CircularProgressIndicator());
           }
           return ListView.builder(
+            controller: _scrollController,
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
               var feedItem = snapshot.data!.docs[index];
@@ -72,6 +91,7 @@ class _FeedTabState extends State<FeedTab> {
                         upvotes: feedItem['upvotes'],
                         upvoters: List<String>.from(feedItem['upvoters'] ?? []),
                         userName: _hideMiddleCharacters(userName),
+                        photoUrl: feedItem['photoUrl'],
                       ));
                     } else {
                       return SizedBox(); // Return empty widget if user data not available
@@ -97,8 +117,7 @@ class _FeedTabState extends State<FeedTab> {
 
   Widget _buildFeedItem(FeedItem feedItem) {
     return Container(
-      margin: EdgeInsets.symmetric(
-          vertical: 2.0), // Tiny vertical space between feed items
+      margin: EdgeInsets.symmetric(vertical: 2.0),
       decoration: BoxDecoration(
         color: Colors.white,
         boxShadow: [
@@ -106,13 +125,15 @@ class _FeedTabState extends State<FeedTab> {
             color: Colors.grey.withOpacity(0.5),
             spreadRadius: 0,
             blurRadius: 2,
-            offset: Offset(0, 2), // Shadow effect
+            offset: Offset(0, 2),
           ),
         ],
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          if (feedItem.photoUrl != null)
+            _buildCachedImage(feedItem.photoUrl!), // Load image from cache
           Padding(
             padding: EdgeInsets.all(12.0),
             child: Text(
@@ -168,6 +189,29 @@ class _FeedTabState extends State<FeedTab> {
     );
   }
 
+  Widget _buildCachedImage(String photoUrl) {
+    // Check if image is already loaded
+    if (_imageCache.containsKey(photoUrl)) {
+      return Image(image: _imageCache[photoUrl]!);
+    } else {
+      // If not, load image and store it in cache
+      return FutureBuilder(
+        future: _loadImage(photoUrl),
+        builder: (context, AsyncSnapshot<ImageProvider> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error loading image'));
+          } else {
+            // Store image in cache
+            _imageCache[photoUrl] = snapshot.data!;
+            return Image(image: snapshot.data!);
+          }
+        },
+      );
+    }
+  }
+
   String _hideMiddleCharacters(String name) {
     int middle = name.length ~/ 2;
     return name.replaceRange(middle - 1, middle + 2, '***');
@@ -207,6 +251,51 @@ class _FeedTabState extends State<FeedTab> {
                   border: OutlineInputBorder(),
                 ),
               ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton.icon(
+                      onPressed: () async {
+                        final pickedFile = await ImagePicker()
+                            .pickImage(source: ImageSource.gallery);
+                        if (pickedFile != null) {
+                          setState(() {
+                            _photoController.text = pickedFile.path!;
+                          });
+                        }
+                      },
+                      icon: Icon(Icons.photo_library),
+                      label: Text('Choose Photo'),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextButton.icon(
+                      onPressed: () async {
+                        final pickedFile = await ImagePicker()
+                            .pickImage(source: ImageSource.camera);
+                        if (pickedFile != null) {
+                          setState(() {
+                            _photoController.text = pickedFile.path!;
+                          });
+                        }
+                      },
+                      icon: Icon(Icons.camera_alt),
+                      label: Text('Take Photo'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _photoController,
+                enabled: false,
+                decoration: InputDecoration(
+                  hintText: 'Photo',
+                  border: OutlineInputBorder(),
+                ),
+              ),
             ],
           ),
           actions: [
@@ -218,15 +307,18 @@ class _FeedTabState extends State<FeedTab> {
               onPressed: () async {
                 var user = _auth.currentUser;
                 if (user != null) {
+                  String? photoUrl = await _uploadPhoto(_photoController.text);
                   await _firestore.collection('feedItems').add({
                     'title': _titleController.text,
                     'description': _descriptionController.text,
                     'upvotes': 0,
                     'upvoters': [],
                     'userId': user.uid,
+                    'photoUrl': photoUrl,
                   });
                   _titleController.clear();
                   _descriptionController.clear();
+                  _photoController.clear();
                   Navigator.of(context).pop();
                 }
               },
@@ -237,6 +329,59 @@ class _FeedTabState extends State<FeedTab> {
       },
     );
   }
+
+  Future<ImageProvider> _loadImage(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return MemoryImage(response.bodyBytes);
+      } else {
+        throw Exception('Failed to load image');
+      }
+    } catch (e) {
+      throw Exception('Failed to load image: $e');
+    }
+  }
+
+  Future<String?> _uploadPhoto(String photoUrl) async {
+    if (photoUrl.isNotEmpty) {
+      try {
+        Uint8List imageData;
+        if (kIsWeb) {
+          http.Response response = await http.get(Uri.parse(photoUrl));
+          imageData = response.bodyBytes;
+        } else {
+          String imagePath = '';
+          if (Platform.isAndroid) {
+            imagePath = await _resolveAndroidContentUri(photoUrl);
+          } else if (Platform.isIOS) {
+            imagePath = await _resolveIOSFilePath(photoUrl);
+          }
+          imageData = await File(imagePath).readAsBytes();
+        }
+        String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+        TaskSnapshot snapshot =
+            await _storage.ref().child('photos/$fileName').putData(imageData);
+        return await snapshot.ref.getDownloadURL();
+      } catch (e) {
+        print('Error uploading photo: $e');
+        return null;
+      }
+    }
+    return null;
+  }
+
+  Future<String> _resolveAndroidContentUri(String uriString) async {
+    final uri = Uri.parse(uriString);
+    final filePath = uri.path;
+    return filePath!;
+  }
+
+  Future<String> _resolveIOSFilePath(String uriString) async {
+    final uri = Uri.parse(uriString);
+    final filePath = uri.path;
+    return filePath!;
+  }
 }
 
 class FeedItem {
@@ -246,6 +391,7 @@ class FeedItem {
   int upvotes;
   final List<String> upvoters;
   final String userName;
+  final String? photoUrl;
 
   FeedItem({
     required this.id,
@@ -254,5 +400,6 @@ class FeedItem {
     this.upvotes = 0,
     required this.upvoters,
     required this.userName,
+    this.photoUrl,
   });
 }
